f79f7fa73aa892c2c3d1d72b3f8f17e85a00a790
brauliorangel0208 <brauliorangel@hotmail.com>
2026-03-18 15:37:06 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 5000,
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 6000,
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 })
